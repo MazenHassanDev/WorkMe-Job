46f34c5b77423d8a6828b65e57767567cfabe4f2
mazen-hassan-dev <fawzymazen1@gmail.com>
2026-04-23 18:35:59 +0100

    fixed login redirecting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useAuth } from './context/AuthContext'
 import './App.css'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 
 function App() {
+  const { accessToken } = useAuth()
+
   return(
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to={accessToken ? "/dashboard": "/login"} />} />
         <Route path='/login' element={<Login /> } />
         <Route path='/register' element={<Register />} />
         <Route path='/dashboard' element={<Dashboard />} />
